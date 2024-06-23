c1976e1e4b2081be6673ead8c3cdb1f4cd909132
ju4nkr <juankr360@gmail.com>
2024-06-23 16:48:29 -0500

    nuevosMetodos

--- ED_P1_Grupo03/src/main/java/ec/edu/espol/ed_p1_grupo03/Usuario.java
+++ ED_P1_Grupo03/src/main/java/ec/edu/espol/ed_p1_grupo03/Usuario.java
@@ -4,17 +4,19 @@
  */
 package ec.edu.espol.ed_p1_grupo03;
 
-import java.util.ArrayList;
-
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.Serializable;
 /**
  *
  * @author jkrom
  */
-public class Usuario {
+public class Usuario implements Serializable {
     private String ID;
     private String password;
-    private String nombre;
-    private ArrayList<Usuario> usuariosRegistrados;
 
     public String getID() {
         return ID;
@@ -31,41 +33,55 @@ public class Usuario {
     public void setpassword(String password) {
         this.password = password;
     }
-
-    public String getNombre() {
-        return nombre;
-    }
-
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
-
-    public ArrayList<Usuario> getUsuariosRegistrados() {
-        return usuariosRegistrados;
-    }
     
 
-    public Usuario(String ID, String password, String nombre) {
+    public Usuario(String ID, String password) {
         this.ID = ID;
         this.password = password;
-        this.nombre = nombre;
     }
-    public void registrarUsuario(Usuario usuario){
-        usuariosRegistrados.add(usuario);
-    }
-    public boolean estaRegistrado(Usuario usuario){
-        if(usuariosRegistrados.isEmpty())
-            return false;
-        for(Usuario user : usuariosRegistrados){
-            if(usuario.getID().equals(user.getID()))
+
+    public static boolean estaRegistrado(LinkedList<Usuario> usuarios, String id) {
+        for (Usuario usuario : usuarios) {
+            if (usuario.getID().equals(id)) {
                 return true;
+            }
         }
-        return false;    
+        return false;
     }
 
     @Override
     public String toString() {
-        return "Usuario{" + "ID=" + ID + ", password=" + password + ", nombre=" + nombre + ", usuariosRegistrados=" + usuariosRegistrados + '}';
+        return ID + "," + password;
     }
-    
+    public static Usuario leerUsuario(String linea) {
+        String[] partes = linea.split(",");
+        if (partes.length == 2) {
+            return new Usuario(partes[0], partes[1]);
+        } else {
+            return null;
+        }
+    }
+    public static void guardarUsuarios(Usuario usuario) {
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter("usuarios.txt"))) {
+                writer.write(usuario.toString());
+                writer.newLine();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    public static LinkedList<Usuario> cargarUsuarios() {
+        LinkedList<Usuario> usuarios = new LinkedList<>();
+        try (BufferedReader reader = new BufferedReader(new FileReader("usuarios.txt"))) {
+            String linea;
+            while ((linea = reader.readLine()) != null) {
+                Usuario usuario = Usuario.leerUsuario(linea);
+                if (usuario != null) {
+                    usuarios.addLast(usuario);
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return usuarios;
+    }
 }
