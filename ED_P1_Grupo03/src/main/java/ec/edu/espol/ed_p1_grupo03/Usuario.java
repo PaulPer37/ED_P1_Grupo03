bf3ea39eba16cb23a9b63bbe58eee963fb844e3f
ju4nkr <juankr360@gmail.com>
2024-06-23 20:14:38 -0500

    UpdateUsuario

--- ED_P1_Grupo03/src/main/java/ec/edu/espol/ed_p1_grupo03/Usuario.java
+++ ED_P1_Grupo03/src/main/java/ec/edu/espol/ed_p1_grupo03/Usuario.java
@@ -6,17 +6,21 @@ package ec.edu.espol.ed_p1_grupo03;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
+import java.io.File;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.Serializable;
+
 /**
  *
  * @author jkrom
  */
 public class Usuario implements Serializable {
+
     private String ID;
     private String password;
+    private LinkedList<Vehiculo> vehiculos;
 
     public String getID() {
         return ID;
@@ -33,11 +37,43 @@ public class Usuario implements Serializable {
     public void setpassword(String password) {
         this.password = password;
     }
-    
+
+    public LinkedList<Vehiculo> getVehiculos() {
+        return vehiculos;
+    }
 
     public Usuario(String ID, String password) {
         this.ID = ID;
         this.password = password;
+        this.vehiculos = new LinkedList<>();
+        cargarVehiculos();
+    }
+
+    private void cargarVehiculos() {
+        String nombreArchivo = "vehiculos" + this.ID + ".txt";
+        File archivo = new File(nombreArchivo);
+        if (archivo.exists()) {
+            vehiculos = Vehiculo.cargarListaCarros(nombreArchivo);
+        }
+    }
+
+    public void agregarVehiculo(Vehiculo vehiculo) {
+        vehiculos.addLast(vehiculo);
+        guardarVehiculos();
+    }
+
+    public void quitarVehiculo(Vehiculo vehiculo) {
+        vehiculos.remove(vehiculo);
+        guardarVehiculos();
+        verificarYEliminarArchivoVehiculos();
+    }
+
+    private void verificarYEliminarArchivoVehiculos() {
+        String nombreArchivo = "vehiculos" + this.ID + ".txt";
+        File archivo = new File(nombreArchivo);
+        if (archivo.length() == 0) {
+            archivo.delete();
+        }
     }
 
     public static boolean estaRegistrado(LinkedList<Usuario> usuarios, String id) {
@@ -49,10 +85,6 @@ public class Usuario implements Serializable {
         return false;
     }
 
-    @Override
-    public String toString() {
-        return ID + "," + password;
-    }
     public static Usuario leerUsuario(String linea) {
         String[] partes = linea.split(",");
         if (partes.length == 2) {
@@ -61,6 +93,19 @@ public class Usuario implements Serializable {
             return null;
         }
     }
+
+    private void guardarVehiculos() {
+        String nombreArchivo = "vehiculos" + this.ID + ".txt";
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(nombreArchivo))) {
+            for (Vehiculo vehiculo : vehiculos) {
+                writer.write(vehiculo.toString());
+                writer.newLine();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
     public static void guardarUsuario(Usuario usuario) {
         try (BufferedWriter writer = new BufferedWriter(new FileWriter("usuarios.txt", true))) {
             writer.write(usuario.toString());
@@ -69,6 +114,7 @@ public class Usuario implements Serializable {
             e.printStackTrace();
         }
     }
+
     public static LinkedList<Usuario> cargarUsuarios() {
         LinkedList<Usuario> usuarios = new LinkedList<>();
         try (BufferedReader reader = new BufferedReader(new FileReader("usuarios.txt"))) {
@@ -84,4 +130,14 @@ public class Usuario implements Serializable {
         }
         return usuarios;
     }
+
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append(ID).append(",").append(password);
+        for (Vehiculo vehiculo : vehiculos) {
+            sb.append(",").append(vehiculo.toString());
+        }
+        return sb.toString();
+    }
+
 }
